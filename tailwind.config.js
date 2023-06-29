8ca14b0c69262bae6669e23645e665755120ec38
Ndifreke <ndifrekesamuel15@gmail.com>
2023-06-29 04:16:37 +0100

    comm

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,15 @@ module.exports = {
       lg: '976px',
       xl: '1440px',
     },
+    fontSize: {
+      sm: '0.8rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    },
     extend: {
       colors: {
         bgColor: '#1f242d',
